d45e98ef7996a03713847fa096b8246b659c690b
DerSandmann-Badcode <maximilian.schwartz@gmail.com>
2018-11-18 13:48:36 +1300

    remove extra usings

--- frmChanThreadWatch.cs
+++ frmChanThreadWatch.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
